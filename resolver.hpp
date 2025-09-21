d6a08eca8a1ae77ada99db508767f345d4435692
imaedat <imaedat@outlook.jp>
2025-09-22 05:14:44 +0900

    add cache to resolver

--- resolver.hpp
+++ resolver.hpp
@@ -1,16 +1,23 @@
 #ifndef RESOLVER_HPP_
 #define RESOLVER_HPP_
 
+#include <arpa/inet.h>
 #include <assert.h>
 #include <netdb.h>
+#include <netinet/in.h>
 #include <poll.h>
 #include <signal.h>
 #include <string.h>
 #include <sys/eventfd.h>
+#include <sys/socket.h>
 
+#include <chrono>
+#include <mutex>
 #include <stdexcept>
 #include <string>
 #include <system_error>
+#include <unordered_map>
+#include <vector>
 
 namespace tbd {
 
@@ -19,6 +26,160 @@ namespace tbd {
  */
 class resolver
 {
+    class cache
+    {
+        static constexpr unsigned CACHE_TTL_SEC = 60 * 60;
+
+        struct string_like_hash
+        {
+            using is_transparent = void;
+            size_t operator()(const std::string& s) const noexcept
+            {
+                return std::hash<std::string>{}(s);
+            }
+            size_t operator()(std::string_view sv) const noexcept
+            {
+                return std::hash<std::string_view>{}(sv);
+            }
+            size_t operator()(const char* s) const noexcept
+            {
+                return std::hash<std::string_view>{}(s);
+            }
+        };
+
+        struct string_like_equal
+        {
+            using is_transparent = void;
+            bool operator()(const std::string& s1, const std::string& s2) const noexcept
+            {
+                return s1 == s2;
+            }
+            bool operator()(const std::string& s1, std::string_view s2) const noexcept
+            {
+                return s1 == s2;
+            }
+            bool operator()(std::string_view s1, const std::string& s2) const noexcept
+            {
+                return s1 == s2;
+            }
+            bool operator()(const std::string& s1, const char* s2) const noexcept
+            {
+                return s1 == s2;
+            }
+            bool operator()(const char* s1, const std::string& s2) const noexcept
+            {
+                return s1 == s2;
+            }
+        };
+
+        struct entry
+        {
+            uint32_t ipaddr;
+            std::chrono::steady_clock::time_point timestamp;
+
+            bool is_expired(int ttl) const noexcept
+            {
+                using namespace std::chrono;
+                return steady_clock::now() - timestamp >= seconds(ttl);
+            }
+        };
+
+      public:
+        static cache& get_instance() noexcept
+        {
+            static cache instance_;
+            return instance_;
+        }
+
+        void clear()
+        {
+            std::lock_guard<decltype(mtx_)> lk(mtx_);
+            cache_.clear();
+        }
+
+        void add_or_replace(std::string_view hostname, uint32_t ipaddr)
+        {
+            std::lock_guard<decltype(mtx_)> lk(mtx_);
+            cache_.insert_or_assign(hostname.data(),
+                                    entry{ipaddr, std::chrono::steady_clock::now()});
+        }
+
+        void add_or_replace(std::string_view hostname, std::string_view ipaddr)
+        {
+            std::lock_guard<decltype(mtx_)> lk(mtx_);
+            cache_.insert_or_assign(hostname.data(), entry{::inet_addr(ipaddr.data()),
+                                                           std::chrono::steady_clock::now()});
+        }
+
+        void remove_by_name(std::string_view hostname)
+        {
+            std::lock_guard<decltype(mtx_)> lk(mtx_);
+            cache_.erase(hostname.data());
+        }
+
+        void remove_by_addr(std::string_view ipaddr)
+        {
+            uint32_t addr = ::inet_addr(ipaddr.data());
+            std::lock_guard<decltype(mtx_)> lk(mtx_);
+            for (auto it = cache_.begin(); it != cache_.end();) {
+                if (it->second.ipaddr == addr) {
+                    it = cache_.erase(it);
+                } else {
+                    ++it;
+                }
+            }
+        }
+
+        int64_t find_addr_by_name(std::string_view hostname)
+        {
+            std::lock_guard<decltype(mtx_)> lk(mtx_);
+            auto it = cache_.find(hostname.data());
+            if (it == cache_.end()) {
+                return -1;
+            }
+
+            if (it->second.is_expired(ttl_)) {
+                cache_.erase(it);
+                return -1;
+            }
+
+            return it->second.ipaddr;
+        }
+
+        std::vector<std::string> find_names_by_addr(std::string_view ipaddr)
+        {
+            return find_names_by_addr(::inet_addr(ipaddr.data()));
+        }
+
+        std::vector<std::string> find_names_by_addr(uint32_t ipaddr)
+        {
+            std::vector<std::string> names;
+            std::lock_guard<decltype(mtx_)> lk(mtx_);
+            for (auto it = cache_.begin(); it != cache_.end();) {
+                if (it->second.ipaddr != ipaddr) {
+                    ++it;
+                } else if (it->second.is_expired(ttl_)) {
+                    it = cache_.erase(it);
+                } else {
+                    names.emplace_back(it->first);
+                    ++it;
+                }
+            }
+            return names;
+        }
+
+        void set_ttl(int new_ttl)
+        {
+            std::lock_guard<decltype(mtx_)> lk(mtx_);
+            ttl_ = new_ttl;
+        }
+
+      private:
+        std::mutex mtx_;
+        std::unordered_map<std::string, entry, string_like_hash, string_like_equal> cache_;
+        int ttl_ = CACHE_TTL_SEC;
+    };  // class cache
+
   public:
     explicit resolver(std::string_view host)
         : resolve_state_(0)
@@ -53,6 +214,11 @@ class resolver
 
     int64_t lookup_nb()
     {
+        auto addr = cache::get_instance().find_addr_by_name(host_);
+        if (addr >= 0) {
+            return addr;
+        }
+
         switch (resolve_state_) {
         case 0: {
             if (int err = ::getaddrinfo_a(GAI_NOWAIT, list_, 1, &sigev_)) {
@@ -68,6 +234,22 @@ class resolver
         default:
             return ipaddr_;
         }
+    }
+
+    template <typename T>
+    void add_cache(std::string_view hostname, T ipaddr) const
+    {
+        cache::get_instance().add_or_replace(hostname, ipaddr);
+    }
+
+    void remove_cached_name(std::string_view hostname) const
+    {
+        cache::get_instance().remove_by_name(hostname);
+    }
+
+    void remove_cached_addr(std::string_view ipaddr) const
+    {
+        cache::get_instance().remove_by_addr(ipaddr);
     }
 
   protected:
@@ -107,6 +289,7 @@ class resolver
         ipaddr_ = ((struct sockaddr_in*)req_.ar_result->ai_addr)->sin_addr.s_addr;
         resolve_state_ = 2;
         free_result();
+        cache::get_instance().add_or_replace(host_, (uint32_t)ipaddr_);
         return ipaddr_;
     }
 
@@ -134,7 +317,7 @@ class resolver
             req_.ar_result = nullptr;
         }
     }
-};
+};  // calss resolver
 
 /**********************************************************************
  * with notifier
