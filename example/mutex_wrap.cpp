6336467a437bad66e68e08706e80e931cb90f209
imaedat <imaedat@outlook.jp>
2026-02-11 04:16:28 +0900

    [mutex_wrap] introduce custom wrapper instead of unique_ptr

--- example/mutex_wrap.cpp
+++ example/mutex_wrap.cpp
@@ -9,48 +9,45 @@
 
 #define THREAD_POOL_ENABLE_WAIT_ALL
 #include "thread_pool.hpp"
+#include "util.h"
 
 using namespace std;
 using namespace std::chrono;
 using namespace tbd;
 
-inline char* now()
-{
-    thread_local char buf[32] = {0};
-
-    auto count = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
-    auto sec = count / (1000 * 1000);
-    auto usec = count % (1000 * 1000);
-    struct tm tm;
-    localtime_r(&sec, &tm);
-    strftime(buf, 21, "%F %T.", &tm);
-    sprintf(buf + 20, "%06ld", usec);
-    return buf;
-}
-
-#define LOG(fmt, ...) printf("%s [%04ld] " fmt, now(), syscall(SYS_gettid), ##__VA_ARGS__)
-
 void normal_mutex(thread_pool& pool)
 {
     mutex_wrap str(string("hello"));
 
-    for (int i = 0; i < 10; ++i) {
-        pool.submit([&, i] {
-            LOG("task-%02d: start\n", i);
-            auto ms = random() % 300;
-            str.lock([&](auto& data) {
-                LOG("task-%02d: acquire resource(%s), work for %lu ms ...\n", i, data.c_str(), ms);
-                usleep(ms * 1000);
-                LOG("task-%02d: finished\n", i);
+    {
+        for (int i = 0; i < 10; ++i) {
+            pool.submit([&, i] {
+                LOG("task-%02d: start\n", i);
+                auto ms = random() % 300;
+                str.lock([&](auto& data) {
+                    LOG("task-%02d: acquire resource(%s), work for %lu ms ...\n", i, data.c_str(),
+                        ms);
+                    usleep(ms * 1000);
+                    LOG("task-%02d: finished\n", i);
+                });
             });
-        });
+        }
+
+        pool.wait_all();
     }
 
-    pool.wait_all();
+    puts("---");
+    {
+        auto g = str.lock();
+        printf("%s\n", g->c_str());
+    }
 
-    unique_lock lk(*str);
-    condition_variable cv;
-    cv.wait_for(lk, milliseconds(1500));
+    puts("---");
+    {
+        unique_lock lk(*str);
+        condition_variable cv;
+        cv.wait_for(lk, milliseconds(500));
+    }
 }
 
 #define is_writer(i) (((i) % 4 == 0))
@@ -59,27 +56,37 @@ void readwrite_mutex(thread_pool& pool)
 {
     rwlock_wrap str(string("world"));
 
-    for (int i = 0; i < 10; ++i) {
-        pool.submit([&, i] {
-            LOG("task-%02d: start\n", i);
-            auto ms = random() % 800;
-            if (is_writer(i)) {
-                auto s = str.lock();
-                LOG("task-%02d: acquire write lock(%s), work for %lu ms ...\n", i, s->c_str(), ms);
-                usleep(ms * 1000);
-                LOG("task-%02d: release write lock\n", i);
-                // s->clear();  // compile ok
-            } else {
-                auto s = str.lock_shared();
-                LOG("task-%02d: acquire read lock(%s), work for %lu ms ...\n", i, s->c_str(), ms);
-                usleep(ms * 1000);
-                LOG("task-%02d: release read lock\n", i);
-                // s->clear();  // compile error
-            }
-        });
+    {
+        for (int i = 0; i < 10; ++i) {
+            pool.submit([&, i] {
+                LOG("task-%02d: start\n", i);
+                auto ms = random() % 800;
+                if (is_writer(i)) {
+                    auto s = str.lock();
+                    LOG("task-%02d: acquire write lock(%s), work for %lu ms ...\n", i, s->c_str(),
+                        ms);
+                    usleep(ms * 1000);
+                    LOG("task-%02d: release write lock\n", i);
+                    // s->clear();  // compile ok
+                } else {
+                    auto s = str.lock_shared();
+                    LOG("task-%02d: acquire read lock(%s), work for %lu ms ...\n", i, s->c_str(),
+                        ms);
+                    usleep(ms * 1000);
+                    LOG("task-%02d: release read lock\n", i);
+                    // s->clear();  // compile error
+                }
+            });
+        }
+
+        pool.wait_all();
     }
 
-    pool.wait_all();
+    puts("---");
+    {
+        auto g = str.lock_shared();
+        printf("%s\n", (*g).c_str());
+    }
 }
 
 int main()
@@ -89,6 +96,6 @@ int main()
     thread_pool pool;
 
     normal_mutex(pool);
-    puts("---");
+    puts("===");
     readwrite_mutex(pool);
 }
--- mutex_wrap.hpp
+++ mutex_wrap.hpp
@@ -13,11 +13,9 @@ namespace tbd {
 template <typename T, typename M = std::mutex>
 class mutex_wrap
 {
-    using Deleter = std::function<void(T*)>;
-
   protected:
     T data_;
-    std::unique_ptr<M> mtx_;
+    std::shared_ptr<M> mtx_;
 
     void assert_mutex_alive() const
     {
@@ -26,28 +24,84 @@ class mutex_wrap
         }
     }
 
+    class guard
+    {
+        T* data_;
+        std::weak_ptr<M> mtx_;
+
+        guard(T* d, std::shared_ptr<M>& m)
+            : data_(d)
+            , mtx_(m)
+        {
+        }
+        friend class mutex_wrap<T, M>;
+
+        void assert_mutex_alive() const
+        {
+            if (mtx_.expired()) {
+                throw std::runtime_error("mutex_wrap: already moved away");
+            }
+        }
+
+      public:
+        guard(const guard&) = delete;
+        guard& operator=(const guard&) = delete;
+        guard(guard&&) noexcept = default;
+        guard& operator=(guard&&) noexcept = delete;
+
+        ~guard()
+        {
+            if (auto sp = mtx_.lock()) {
+                sp->unlock();
+            }
+        };
+
+        T* get() const
+        {
+            assert_mutex_alive();
+            return data_;
+        }
+
+        T* operator->() const
+        {
+            return get();
+        }
+
+        T& operator*() const
+        {
+            assert_mutex_alive();
+            return *data_;
+        }
+    };
+
+    static guard make_guard(T* d, std::shared_ptr<M>& m)
+    {
+        return guard(d, m);
+    }
+
   public:
     mutex_wrap(T&& data)
-        : data_(std::move(data))
-        , mtx_(std::make_unique<M>())
+        : data_(std::forward<T>(data))
+        , mtx_(std::make_shared<M>())
     {
     }
 
+    mutex_wrap(const mutex_wrap&) = delete;
+    mutex_wrap& operator=(const mutex_wrap&) = delete;
+
     virtual ~mutex_wrap() noexcept = default;
 
-    [[nodiscard]] std::unique_ptr<T, Deleter> lock()
+    [[nodiscard]] guard lock()
     {
         assert_mutex_alive();
-
         mtx_->lock();
-        return std::unique_ptr<T, Deleter>(&data_, [this](T*) { mtx_->unlock(); });
+        return guard(&data_, mtx_);
     }
 
     template <typename F, typename = std::enable_if_t<std::is_invocable_v<F, T&>>>
     void lock(F&& fn)
     {
         assert_mutex_alive();
-
         std::lock_guard<M> lk(*mtx_);
         fn(data_);
     }
@@ -61,28 +115,23 @@ class mutex_wrap
 template <typename T>
 class rwlock_wrap : public mutex_wrap<T, std::shared_mutex>
 {
-    using Deleter = std::function<void(const T*)>;
-
   public:
     rwlock_wrap(T&& data)
         : mutex_wrap<T, std::shared_mutex>(std::move(data))
     {
     }
 
-    [[nodiscard]] std::unique_ptr<const T, Deleter> lock_shared()
+    [[nodiscard]] typename mutex_wrap<T, std::shared_mutex>::guard lock_shared()
     {
         this->assert_mutex_alive();
-
         this->mtx_->lock_shared();
-        return std::unique_ptr<const T, Deleter>(&this->data_,
-                                                 [this](const T*) { this->mtx_->unlock_shared(); });
+        return this->make_guard(&this->data_, this->mtx_);
     }
 
     template <typename F, typename = std::enable_if_t<std::is_invocable_v<F, const T&>>>
     void lock_shared(F&& fn)
     {
         this->assert_mutex_alive();
-
         std::shared_lock<std::shared_mutex> lk(*this->mtx_);
         fn(this->data_);
     }
