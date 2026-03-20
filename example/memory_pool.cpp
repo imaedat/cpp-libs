a7ac71af5379903a25687dc7241b2c107dbaa0e3
imaedat <imaedat@outlook.jp>
2026-03-20 20:54:29 +0900

    [memory_pool] fix

--- example/memory_pool.cpp
+++ example/memory_pool.cpp
@@ -3,10 +3,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <chrono>
+
+#include "logger.hpp"
 #define THREAD_POOL_ENABLE_WAIT_ALL
 #include "thread_pool.hpp"
 
 using namespace std;
+using namespace std::chrono;
 using namespace tbd;
 
 void with_mutex()
@@ -14,13 +18,14 @@ void with_mutex()
     // aligns up to 256B
     memory_pool pool(250, 120);
     vector<void*> borders;
+    logger logger("with_mutex", "/dev/stdout");
 
     for (size_t i = 0; i < 121; ++i) {
         auto* p = pool.acquire();
         if (i < 4 || (60 < i && i < 69) || 116 < i) {
-            printf("[%03lu] addr=%p\n", i, p);
+            logger.info("[%03lu] addr=%p", i, p);
         } else if (i == 4 || i == 116) {
-            puts("---");
+            logger.info("---");
         }
         if (60 < i && i < 69) {
             borders.push_back(p);
@@ -31,11 +36,11 @@ void with_mutex()
         pool.release(p);
     }
 
-    puts("===");
+    logger.info("===");
 
     for (size_t i = 0; i < 8; ++i) {
         auto* p = pool.acquire();
-        printf("[%03lu] addr=%p\n", i, p);
+        logger.info("[%03lu] addr=%p", i, p);
     }
 }
 
@@ -44,30 +49,92 @@ void with_lockfree()
     size_t nworkers = 16;
     memory_pool_lf mpool(250, 4);
     thread_pool tpool(8);
+    logger logger("with_lockfree", "/dev/stdout");
+    // logger.set_level("debug");
+
+    {
+        auto mp2 = move(mpool);
+        memory_pool_lf mp3(1, 1);
+        mp3 = move(mp2);
+        mpool = move(mp3);
+    }
 
     for (size_t i = 0; i < nworkers; ++i) {
-        tpool.submit([i, &mpool] {
+        tpool.submit([i, &mpool, &logger] {
             int r = 0;
             auto t = random() % (1000 * 1000);
         again:
             auto* p = mpool.acquire();
             if (!p) {
-                printf("worker-%02lu: cannot acquired, retry (%d)\n", i, ++r);
+                logger.debug("worker-%02lu: cannot acquired, retry (%d)", i, ++r);
                 usleep(10 * 1000);
-                // usleep(500 * 1000);
                 goto again;
             }
-            printf("worker-%02lu: acquired %p, work for %lu ms ...\n", i, p, t);
+            logger.info("worker-%02lu: acquired %p, work for %lu ms ...", i, p, t);
             usleep(t);
-            printf("worker-%02lu: release  %p\n", i, p);
+            logger.info("worker-%02lu: release  %p", i, p);
             mpool.release(p);
         });
     }
     tpool.wait_all();
+}
 
-    auto mp2 = std::move(mpool);
-    memory_pool_lf mp3(1, 1);
-    mp3 = std::move(mp2);
+void bench()
+{
+    constexpr size_t THREADS = 8;
+    constexpr size_t ITERS = 10000;
+    constexpr size_t BLOCKSZ = 4096;
+    constexpr size_t COUNT = 7;  // 8
+    constexpr unsigned WORK_US = 0;
+
+    thread_pool thrpool(THREADS);
+
+    {
+        memory_pool pool(BLOCKSZ, COUNT);
+        auto t = steady_clock::now();
+        for (size_t i = 0; i < THREADS; ++i) {
+            thrpool.submit([&pool] {
+                for (size_t j = 0; j < ITERS; ++j) {
+                    auto* p = pool.acquire();
+                    if (WORK_US > 0) {
+                        usleep(WORK_US);
+                    }
+                    pool.release(p);
+                }
+            });
+        }
+
+        thrpool.wait_all();
+        printf("mt: elapsed %lu us\n",
+               duration_cast<microseconds>(steady_clock::now() - t).count());
+    }
+
+    {
+        atomic<size_t> retry{0};
+        memory_pool_lf pool(BLOCKSZ, COUNT);
+        auto t = steady_clock::now();
+        for (size_t i = 0; i < THREADS; ++i) {
+            thrpool.submit([&pool, &retry] {
+                for (size_t j = 0; j < ITERS; ++j) {
+                again:
+                    auto* p = pool.acquire();
+                    if (!p) {
+                        retry.fetch_add(1, memory_order_relaxed);
+                        this_thread::yield();
+                        goto again;
+                    }
+                    if (WORK_US > 0) {
+                        usleep(WORK_US);
+                    }
+                    pool.release(p);
+                }
+            });
+        }
+
+        thrpool.wait_all();
+        printf("lf: elapsed %lu us (#retries = %lu)\n",
+               duration_cast<microseconds>(steady_clock::now() - t).count(), retry.load());
+    }
 }
 
 int main()
@@ -79,4 +146,8 @@ int main()
     puts("---");
 
     with_lockfree();
+
+    puts("---");
+
+    bench();
 }
--- memory_pool.hpp
+++ memory_pool.hpp
@@ -3,42 +3,56 @@
 
 #include <atomic>
 #include <cassert>
+#include <cstddef>
+#include <memory>
 #include <mutex>
 #include <vector>
 
 namespace tbd {
 
 namespace detail {
-template <typename T>
+template <typename T, size_t AlignAs = alignof(std::max_align_t)>
 class memory_pool_base
 {
+    static constexpr size_t ceil2ll(size_t n)
+    {
+        return (n <= 1) ? 1 : size_t(1) << (sizeof(size_t) * 8 - __builtin_clzll(n - 1));
+    }
+
   public:
     memory_pool_base(const memory_pool_base&) = delete;
     memory_pool_base& operator=(const memory_pool_base&) = delete;
     memory_pool_base(memory_pool_base&&) noexcept = default;
     memory_pool_base& operator=(memory_pool_base&& rhs) noexcept = default;
     virtual ~memory_pool_base() noexcept = default;
-    virtual void* acquire() = 0;
-    virtual void release(void* p) = 0;
 
   protected:
-    // align of 8
-    static constexpr size_t ALIGN = 8U;
-    static constexpr size_t WORDSZ = sizeof(uint64_t) * 8;
+    static constexpr size_t ALIGN_AS = ceil2ll(AlignAs);
+    static constexpr size_t WORD_BITS = sizeof(uint64_t) * 8;
+    struct aligned_deleter
+    {
+        void operator()(void* p) const noexcept
+        {
+            ::operator delete (p, std::align_val_t{ALIGN_AS});
+        }
+    };
 
-    size_t block_size_;
-    std::vector<uint8_t> pool_;
+    size_t block_size_ = 0;
+    size_t pool_size_ = 0;
+    std::unique_ptr<std::byte, aligned_deleter> pool_;
     std::vector<T> avail_map_;
 
     memory_pool_base(size_t blksz, size_t count)
-        : block_size_((blksz + ALIGN - 1) & ~(ALIGN - 1))
-        , pool_(block_size_ * count)
-        , avail_map_((count + (WORDSZ - 1)) / WORDSZ)
+        : block_size_((blksz + ALIGN_AS - 1) & ~(ALIGN_AS - 1))
+        , pool_size_(block_size_ * count)
+        , pool_((std::byte*)::operator new (pool_size_, std::align_val_t{ALIGN_AS}),
+                aligned_deleter{})
+        , avail_map_((count + (WORD_BITS - 1)) / WORD_BITS)
     {
         for (size_t i = 0; i < avail_map_.size(); ++i) {
             avail_map_[i] = 0xffffffffffffffffULL;
         }
-        if (auto rem = count % WORDSZ) {
+        if (auto rem = count % WORD_BITS) {
             avail_map_.back() = (1ULL << rem) - 1;
         }
     }
@@ -49,46 +63,47 @@ class memory_pool_base
             return {0, 0};
         }
 
-        if (ptr < pool_.data() || ptr >= pool_.data() + pool_.size()) {
-            throw std::system_error(EINVAL, std::generic_category());
+        if (ptr < pool_.get() || ptr >= pool_.get() + pool_size_) {
+            throw std::out_of_range("index_relbit: invalid pointer");
         }
 
-        auto idx = ((uint8_t*)ptr - pool_.data()) / block_size_;
-        return {idx / WORDSZ, 1ULL << (idx & (WORDSZ - 1))};
+        auto idx = ((std::byte*)ptr - pool_.get()) / block_size_;
+        return {idx / WORD_BITS, 1ULL << (idx & (WORD_BITS - 1))};
     }
 };
 }  // namespace detail
 
-class memory_pool : public detail::memory_pool_base<uint64_t>
+template <size_t AlignAs = alignof(std::max_align_t)>
+class memory_pool : public detail::memory_pool_base<uint64_t, AlignAs>
 {
   public:
     memory_pool(size_t blksz, size_t count)
-        : detail::memory_pool_base<uint64_t>(blksz, count)
+        : detail::memory_pool_base<uint64_t, AlignAs>(blksz, count)
     {
     }
 
-    void* acquire() override
+    void* acquire()
     {
         std::lock_guard lk(mtx_);
-
-        for (size_t i = 0; i < avail_map_.size(); ++i) {
-            if (avail_map_[i]) {
-                auto pos = __builtin_ffsll(avail_map_[i]) - 1;
-                avail_map_[i] &= ~(1ULL << pos);
-                auto ptr = pool_.data() + block_size_ * (i * WORDSZ + pos);
-                assert(ptr < pool_.data() + pool_.size());
+        for (size_t i = 0; i < this->avail_map_.size(); ++i) {
+            if (this->avail_map_[i]) {
+                auto pos = __builtin_ffsll(this->avail_map_[i]) - 1;
+                this->avail_map_[i] &= ~(1ULL << pos);
+                auto ptr = this->pool_.get() + this->block_size_ * (i * this->WORD_BITS + pos);
+                assert(ptr < this->pool_.get() + this->pool_size_);
                 return ptr;
             }
         }
         return nullptr;
     }
 
-    void release(void* ptr) override
+    void release(void* ptr)
     {
-        auto [index, relbit] = index_relbit(ptr);
+        auto [index, relbit] = this->index_relbit(ptr);
         if (relbit) {
             std::lock_guard lk(mtx_);
-            avail_map_[index] |= relbit;
+            assert(!(this->avail_map_[index] & relbit));
+            this->avail_map_[index] |= relbit;
         }
     }
 
@@ -96,37 +111,62 @@ class memory_pool : public detail::memory_pool_base<uint64_t>
     std::mutex mtx_;
 };
 
-class memory_pool_lf : public detail::memory_pool_base<std::atomic<uint64_t>>
+template <size_t AlignAs = alignof(std::max_align_t)>
+class memory_pool_lf : public detail::memory_pool_base<std::atomic<uint64_t>, AlignAs>
 {
   public:
     memory_pool_lf(size_t blksz, size_t count)
-        : detail::memory_pool_base<std::atomic<uint64_t>>(blksz, count)
+        : detail::memory_pool_base<std::atomic<uint64_t>, AlignAs>(blksz, count)
     {
     }
-
-    void* acquire() override
+    memory_pool_lf(memory_pool_lf&& rhs)
+        : detail::memory_pool_base<std::atomic<uint64_t>, AlignAs>(std::move(rhs))
+        , hint_(rhs.hint_.load(std::memory_order_relaxed))
     {
-        for (size_t i = 0; i < avail_map_.size(); ++i) {
-            auto old_val = avail_map_[i].load(std::memory_order_relaxed);
+    }
+    memory_pool_lf& operator=(memory_pool_lf&& rhs)
+    {
+        if (this != &rhs) {
+            detail::memory_pool_base<std::atomic<uint64_t>, AlignAs>::operator=(std::move(rhs));
+            hint_.store(rhs.hint_.load(std::memory_order_relaxed), std::memory_order_relaxed);
+        }
+        return *this;
+    }
+
+    void* acquire()
+    {
+        auto i = hint_.load(std::memory_order_relaxed);
+        for (; i < this->avail_map_.size(); ++i) {
+            auto old_val = this->avail_map_[i].load(std::memory_order_relaxed);
             while (old_val) {
                 auto pos = __builtin_ffsll(old_val) - 1;
-                if (avail_map_[i].compare_exchange_weak(old_val, old_val & ~(1ULL << pos),
-                                                        std::memory_order_acquire,
-                                                        std::memory_order_relaxed)) {
-                    return pool_.data() + block_size_ * (i * WORDSZ + pos);
+                if (this->avail_map_[i].compare_exchange_weak(old_val, old_val & ~(1ULL << pos),
+                                                              std::memory_order_acq_rel,
+                                                              std::memory_order_relaxed)) {
+                    hint_.store(i, std::memory_order_relaxed);
+                    auto ptr = this->pool_.get() + this->block_size_ * (i * this->WORD_BITS + pos);
+                    assert(ptr < this->pool_.get() + this->pool_size_);
+                    return ptr;
                 }
             }
         }
         return nullptr;
     }
 
-    void release(void* ptr) override
+    void release(void* ptr)
     {
-        auto [index, relbit] = index_relbit(ptr);
+        auto [index, relbit] = this->index_relbit(ptr);
         if (relbit) {
-            avail_map_[index] |= relbit;
+            auto old_val = this->avail_map_[index].fetch_or(relbit, std::memory_order_release);
+            assert(!(old_val & relbit));
+            if (index < hint_.load(std::memory_order_relaxed)) {
+                hint_.store(index, std::memory_order_relaxed);
+            }
         }
     }
+
+  private:
+    std::atomic<size_t> hint_{0};
 };
 
 }  // namespace tbd
